87ccfa5e603334aa3a020ebee6e0008d5c9cfa22
Thi Thu Ha Nguyen <tnguy655@my.centennialcollege.ca>
2020-10-27 20:40:07 -0400

    complete post route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,3 +1,4 @@
+const { json } = require('body-parser');
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
@@ -80,12 +81,89 @@ router.post('/like/:id', passport.authenticate('jwt', { session: false }), (req,
                     if (post.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
                         return res.status(400).json({ alreadyliked: 'User already liked this post' })
                     }
-
+                    //push user to likes list
                     post.likes.unshift({ user: req.user.id });
                     post.save().then(post => res.json(post));
                 })
         })
 })
 
+//@Route    POST api/posts/unlike/:id
+//@desc     Unlike post
+//@access   Private
+router.post('/unlike/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+    Profile.findOne({ user: req.user.id })
+        .then(profile => {
+            Post.findById(req.params.id)
+                .then(post => {
+                    if (post.likes.filter(like => like.user.toString() === req.user.id).length === 0) {
+                        return res.status(400).json({ notliked: 'You have not yet liked the post' })
+                    }
+
+                    //get remove index
+                    const removedIndex = post.likes
+                        .map(item => item.user.toString())
+                        .indexOf(req.user.id);
+
+                    //splice out of 
+                    post.likes.splice(removedIndex, 1);
+
+                    //save 
+                    post.save().then(post => res.json(post));
+                })
+        })
+})
+
+//@Route    POST api/posts/comment/:id
+//@desc     Add comment to post
+//@access   Private
+router.post('/comment/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    //check validation
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
+    Post.findById(req.params.id)
+        .then(post => {
+            const newComment = {
+                text: req.body.text,
+                name: req.body.name,
+                avatar: req.body.avatar,
+                user: req.user.id
+            }
+
+            //add to comments array
+            post.comments.unshift(newComment);
+
+            //save
+            post.save().then(post => res.json(post))
+                .catch(err => res.status(404).json({ postnotfound: 'No post found' }))
+        })
+})
+
+//@Route    DELETE api/posts/comment/:id/:comment_id
+//@desc     Remove comment from post
+//@access   Private
+router.delete('/comment/:id/:comment_id', passport.authenticate('jwt', { session: false }), (req, res) => {
+    Post.findById(req.params.id)
+        .then(post => {
+            if (post.comments.filter(comment => comment._id.toString() === req.params.comment_id).length === 0) {
+                return res.status(404).json({ commentNotExist: 'Comment does not exist' });
+            }
+
+            //get remove index
+            const removeIndex = post.comments.map(item => item._id.toString())
+                .indexOf(req.params.comment_id);
+
+            //remove
+            post.comments.splice(removeIndex, 1);
+
+            //save
+            post.save().then(post => res.json(post));
+        })
+        .catch(err => res.status(404).json({ postnotfound: 'Post not found' }));
+})
 
 module.exports = router;
